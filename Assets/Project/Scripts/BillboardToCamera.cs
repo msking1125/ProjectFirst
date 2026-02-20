d7511a7493a478ae0e98421d9b3bd8e883f523e6
김민석 <msking1125@gmail.com>
2026-02-21 00:39:43 +0900

    (fix) 데미지 폰트 수정, 오더 레이어 태그 UI 추가

---
+++ Assets/Project/Scripts/BillboardToCamera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardToCamera : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        if (Camera.main == null) return;
+        transform.forward = Camera.main.transform.forward;
+    }
+}
